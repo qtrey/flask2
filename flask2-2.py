ece884098b07152b7c51feea8729d89b30d1430e
qtrey <kimhyoungkyun25@gmail.com>
2025-01-08 09:42:08 +0900

    done

---
+++ flask2-2.py
@@ -0,0 +1,11 @@
+from marshmallow import Schema, fields
+
+class BookSchema(Schema):
+from marshmallow import Schema, fields
+
+class BookSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.String(required=True)
+    author = fields.String(required=True)    id = fields.Int(dump_only=True)
+    title = fields.String(required=True)
+    author = fields.String(required=True)
